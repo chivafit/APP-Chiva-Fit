6c3e4efda5179751bcc2c4a3e33cf924799bdd2b
chivafit <chivafit@users.noreply.github.com>
2026-03-16 17:31:43 -0300

    Chore: bump cache (v=20260316-2)

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { allInsumos, allOrdens, getEstPct, getEstStatus } from "./producao.js?v=20260313-3";
+import { allInsumos, allOrdens, getEstPct, getEstStatus } from "./producao.js?v=20260316-2";
 import {
   computeCustomerIntelligence as computeCustomerIntelligenceImpl,
   definirNextBestAction as definirNextBestActionImpl,
@@ -8,11 +8,11 @@ import {
   runAI as runAIImpl,
   copyWhatsAppMessageForCustomer as copyWhatsAppMessageForCustomerImpl,
   openWhatsAppForCustomer as openWhatsAppForCustomerImpl
-} from "./ia.js?v=20260313-3";
-import { escapeHTML, safeJsonParse, escapeJsSingleQuote, safeSetItem } from "./utils.js?v=20260313-3";
-import { CRMStore } from "./store.js?v=20260313-3";
-import { STORAGE_KEYS } from "./constants.js?v=20260313-3";
-import { getSupabaseClient } from "./supabaseClient.js?v=20260313-3";
+} from "./ia.js?v=20260316-2";
+import { escapeHTML, safeJsonParse, escapeJsSingleQuote, safeSetItem } from "./utils.js?v=20260316-2";
+import { CRMStore } from "./store.js?v=20260316-2";
+import { STORAGE_KEYS } from "./constants.js?v=20260316-2";
+import { getSupabaseClient } from "./supabaseClient.js?v=20260316-2";
 import {
   getDashboardKpis as getDashboardKpisView,
   getDashboardDaily as getDashboardDailyView,
@@ -25,18 +25,18 @@ import {
   getClientesSemContato as getClientesSemContatoView,
   getClientesInteligencia as getClientesInteligenciaView,
   normalizeClienteIntel
-} from "./viewsApi.js?v=20260313-3";
+} from "./viewsApi.js?v=20260316-2";
 import {
   scheduleAutoBlingSync as scheduleAutoBlingSyncImpl,
   syncBling as syncBlingImpl,
   syncBlingProdutos as syncBlingProdutosImpl,
   backfillBlingEnderecos as backfillBlingEnderecosImpl
-} from "./sync/bling.js?v=20260313-3";
+} from "./sync/bling.js?v=20260316-2";
 import {
   syncYampi as syncYampiImpl,
   syncCarrinhosAbandonadosYampi as syncCarrinhosAbandonadosYampiImpl,
   scheduleAutoCarrinhosSync as scheduleAutoCarrinhosSyncImpl
-} from "./sync/yampi.js?v=20260313-3";
+} from "./sync/yampi.js?v=20260316-2";
 
 document.addEventListener("DOMContentLoaded",function(){
   if(window.Chart){
--- js/main.js
+++ js/main.js
@@ -1,2 +1,2 @@
-import "../app.js?v=20260313-3";
-import "../controller.js?v=20260313-3";
+import "../app.js?v=20260316-2";
+import "../controller.js?v=20260316-2";
